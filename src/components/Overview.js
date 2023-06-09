9303d2de6154b7efdd4b89e641baa2a37b46147a
ibrahimfe <ibrahiimm.f@gmail.com>
2023-06-09 15:37:00 +0700

    adding Hero.js Feature1.js Brands.js Overview.js App.js

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,22 @@
+//import React from 'react';
+
+//const Overview = () => {
+//  return <div>Overview</div>;
+//};
+
+//export default Overview;
 import React from 'react';
+import {overview} from '../data';
 
-const Overview = () => {
-  return <div>Overview</div>;
+const Overview = ()=> {
+    const {productImg} = overview;
+    return(
+        <section className='lg:min-h-[712px]bg-overview bg-cover bg-left-top pt-[30px] lg:pt-[87px]'>
+            <div className='container mx-auto flex justfify-end overlow-hidden'>
+                <img src={productImg} alt='' data-aos='fade-up' data-aos-offset='300'/>
+            </div>
+
+        </section>
+    );
 };
-
 export default Overview;
