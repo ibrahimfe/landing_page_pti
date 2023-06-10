cb3b779a92d5ef2a43150a24b44a0e80c154386b
ibrahimfe <ibrahiimm.f@gmail.com>
2023-06-10 10:36:23 +0700

    add Testimonials and ClientSlider

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import { testimonials } from '../data';
+
+import ClientSlider from '../components/ClientSlider';
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const { title, clients } = testimonials;
+  return <section className='section'>
+    <div className="container mx-auto">
+      <h2 className='title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto'
+        data-aos='fade-up' data-aos-delay='200'>
+        {title}
+      </h2>
+      <div data-aos='fade-up' data-aos-delay='400'>
+        <ClientSlider clients={clients} />
+      </div>
+    </div>
+  </section>
 };
 
 export default Testimonials;
