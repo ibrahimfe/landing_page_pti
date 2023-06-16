2c8de4dc276ff723a293f58801f1902cd97ad097
ibrahimfe <ibrahiimm.f@gmail.com>
2023-06-16 14:24:53 +0700

    adding Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,69 +1,85 @@
 import React from "react";
 // import data
 import { footer } from "../data";
+import Copyright from "./Copyright";
 
 const Footer = () => {
   // destructure footer data
   const { logo, links, newsletter, legal, form } = footer;
   return (
-    <footer>
-      <div>
-        {/* Logo */}
-        <div>
-          <img src={logo} alt="" />
-        </div>
-        {/* list 1 */}
-        <div>
-          <div className="text-2xl uppercase font-medium mb-6">Links</div>
-          <ul className="flex flex-col gap-y-3 ">
-            {links.map((item, index) => {
-              // destructure the link
-              const { href, name } = item;
-              return (
-                <li key={index}>
-                  <a
-                    className="font-medium hover:text-accent transition"
-                    href={href}
-                  >
-                    {name}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        {/* list 2 */}
-        <div>
-          <div className="text-2xl uppercase font-medium mb-6">Legal</div>
-          <ul className="flex flex-col gap-y-3 ">
-            {legal.map((item, index) => {
-              // destructure the link
-              const { href, name } = item;
-              return (
-                <li key={index}>
-                  <a
-                    className="font-medium hover:text-accent transition"
-                    href={href}
-                  >
-                    {name}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        {/* newsletter */}
-        <div>
-          <div>{newsletter.title}</div>
-          <div>{newsletter.subtitle}</div>
-          {/* form */}
-          <form className="bg-blue-300 max-w-[394px] mb-[10px]:">
-            <div className="h-[62px] p-[7px] flex border border-dark rounded-">
-              <input type="text" placeholder={form.placeholder} />
-              <button>{form.btnText}</button>
+    <footer className="pt-[142px] pb-[60px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center text-center lg:flex-row lg:items-start lg:text-left lg:justify-between gap-y-8">
+          {/* Logo */}
+          <div data-aos="fade-up" data-aos-offset="200" data-aos-delay="300">
+            <img src={logo} alt="" />
+          </div>
+          {/* list 1 */}
+          <div data-aos="fade-up" data-aos-offset="200" data-aos-delay="400">
+            <div className="text-2xl uppercase font-medium mb-6">Links</div>
+            <ul className="flex flex-col gap-y-3 ">
+              {links.map((item, index) => {
+                // destructure the link
+                const { href, name } = item;
+                return (
+                  <li key={index}>
+                    <a
+                      className="font-medium hover:text-accent transition"
+                      href={href}
+                    >
+                      {name}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          {/* list 2 */}
+          <div data-aos="fade-up" data-aos-offset="200" data-aos-delay="500">
+            <div className="text-2xl uppercase font-medium mb-6">Legal</div>
+            <ul className="flex flex-col gap-y-3 ">
+              {legal.map((item, index) => {
+                // destructure the link
+                const { href, name } = item;
+                return (
+                  <li key={index}>
+                    <a
+                      className="font-medium hover:text-accent transition"
+                      href={href}
+                    >
+                      {name}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          {/* newsletter */}
+          <div data-aos="fade-up" data-aos-offset="200" data-aos-delay="600">
+            <div className="text-2xl uppercase font-medium mb-6">
+              {newsletter.title}
             </div>
-          </form>
+            <div className="text-xl text-light mb-[18px]">
+              {newsletter.subtitle}
+            </div>
+            {/* form */}
+            <form className="max-w-[394px] mb-[10px]:">
+              <div className="h-[62px] p-[7px] flex border border-dark rounded-">
+                <input
+                  className="w-full h-full pl-6 border-none outline-none placeholder:text-dark"
+                  type="text"
+                  placeholder={form.placeholder}
+                />
+                <button className="btn btn-sm bg-accent hover:bg-accentHover w-[102px] text-white">
+                  {form.btnText}
+                </button>
+              </div>
+            </form>
+            <span className="text-sm text-light ">{form.smallText}</span>
+          </div>
         </div>
+        <hr className="mt-10 mb-5" data-aos="fade-up" data-aos-delay="200" />
+        <Copyright />
       </div>
     </footer>
   );
