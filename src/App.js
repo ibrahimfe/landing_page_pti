d586fce83f27e4dd5bf802351d49c4e0355e490b
ibrahimfe <ibrahiimm.f@gmail.com>
2023-06-16 14:25:54 +0700

    removing fix viewport for webpage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React from "react";
 
 // import aos
-import Aos from 'aos';
+import Aos from "aos";
 // import aos css
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 
 // import components
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Overview from './components/Overview';
-import Brands from './components/Brands';
-import Feature1 from './components/Feature1';
-import Feature2 from './components/Feature2';
-import Feature3 from './components/Feature3';
-import Product from './components/Product';
-import Pricing from './components/Pricing';
-import Testimonials from './components/Testimonials';
-import Cta from './components/Cta';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Overview from "./components/Overview";
+import Brands from "./components/Brands";
+import Feature1 from "./components/Feature1";
+import Feature2 from "./components/Feature2";
+import Feature3 from "./components/Feature3";
+import Product from "./components/Product";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Cta from "./components/Cta";
+import Footer from "./components/Footer";
 
 const App = () => {
   // initialize aos
   Aos.init({
     duration: 1800,
     offset: 100,
-});
+  });
   return (
-    <div className='overflow-hidden'>
+    <div className="overflow-hidden">
       <Header />
       <Hero />
       <Overview />
@@ -39,7 +39,7 @@ const App = () => {
       <Testimonials />
       <Cta />
       <Footer />
-      <div className='h-[4000px]'></div>
+      {/* <div className='h-[4000px]'></div> */}
     </div>
   );
 };
